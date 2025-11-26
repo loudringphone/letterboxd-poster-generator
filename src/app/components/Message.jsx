b93ccdaffc56c5b108c27c90ab3f12dd8d9b64e6
Wing Fung Lau <z5011525@ad.unsw.edu.au>
2025-11-26 22:14:32 +1100

    add api message

--- src/app/components/Message.jsx
+++ src/app/components/Message.jsx
@@ -4,6 +4,11 @@ export const Message = ({className}) => {
       <p>
         First, export your Letterboxd account data in CSV format from <a href="https://letterboxd.com/settings/data/" target="_blank" rel="noopener noreferrer">Letterboxd Data Settings</a>. Then, import the diary.csv to generate your monthly film posters.
       </p>
+      <p className="mt-2">
+        To fetch movie posters, you need your own APIs:{' '}
+        <a href="https://www.omdbapi.com/" target="_blank" rel="noopener noreferrer">OMDb API</a> and{' '}
+        <a href="https://rapidapi.com/apidojo/api/imdb8" target="_blank" rel="noopener noreferrer">IMDb8 API</a>.
+      </p>
     </div>
   )
 }
